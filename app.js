e866842ca38d6fcee63620e53b39f2ccfac084d0
ShaRaya Latrese Scovil <sharayalatresescovil@ShaRayas-MBP.hitronhub.home>
2022-02-20 00:52:54 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    document.querySelector("#callback-btn")
+        .addEventListener("click", function () {
+        console.log("User has clicked on the button!")
+    });
+}
